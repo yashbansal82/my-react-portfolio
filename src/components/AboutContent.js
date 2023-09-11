710496555e3dcc45063ec892d52b00c7cd2cac8c
unknown <bansalyash569@gmail.com>
2023-09-11 17:53:04 +0530

    first commit

---
+++ src/components/AboutContent.js
@@ -0,0 +1,32 @@
+import "./AboutContentStyles.css";
+
+import React from 'react';
+import {Link} from "react-router-dom";
+import React1 from "../assets/project6.jpeg";
+import React2 from "../assets/project7.jpg";
+
+const AboutContent = () => {
+  return (
+    <div className="about">
+        <div className="left">
+            <h1>Who Am I?</h1>
+            <p>Im a react font-end developer. I create responsive secure website for my clients.</p>
+            <Link to="/contact">
+                <button className="btn"> Contact</button>
+            </Link>
+        </div>
+        <div className="right">
+            <div className="img-container">
+                <div className="img-stack top">
+                    <img src={React1} className="img" alt="true"/>
+                </div>
+                <div className="img-stack bottom">
+                    <img src={React2} className="img" alt="true"/>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AboutContent;
---
+++ src/components/WorkCardData.js
@@ -0,0 +1,29 @@
+import pro1 from "../assets/project3.png";
+import pro2 from "../assets/project4.jpg";
+import pro3 from "../assets/project5.jpg";
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Ecommerce Website Design",
+        text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of 'Wow! really can do this' for over 100,000 online entrepreneurs. Best selling marketing courses",
+        view: "https://youtu.be/0h2b4ftbZcU?si=63J7k-Q8PN6ymTJZ",
+        source:"https://youtu.be/0h2b4ftbZcU?si=63J7k-Q8PN6ymTJZ"
+    },
+    {
+        imgsrc: pro2,
+        title: "LMS/Courses Website Design",
+        text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of 'Wow! really can do this' for over 100,000 online entrepreneurs. Best selling marketing courses",
+        view: "https://youtu.be/0h2b4ftbZcU?si=63J7k-Q8PN6ymTJZ",
+        source:"https://youtu.be/0h2b4ftbZcU?si=63J7k-Q8PN6ymTJZ"
+    },
+    {
+        imgsrc: pro3,
+        title: "Website Design",
+        text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of 'Wow! really can do this' for over 100,000 online entrepreneurs. Best selling marketing courses",
+        view: "https://youtu.be/0h2b4ftbZcU?si=63J7k-Q8PN6ymTJZ",
+        source:"https://youtu.be/0h2b4ftbZcU?si=63J7k-Q8PN6ymTJZ"
+    },
+];
+
+export default ProjectCardData;
